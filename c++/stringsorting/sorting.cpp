0114ab5ddbd6510c226d5834bf42d276b830fc3f
JannisHessel <jannishessel@gmx.de>
2018-05-23 09:58:01 +0200

    did you get your satisfaction?

---
+++ c++/stringsorting/sorting.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+
+
+int main () {
+
+	string temp = "äÄa";
+	int a = temp.size();
+	int d = -61;int f = -92;
+	char g = d ; char h = f ;
+	for (int i=0;i<8;i++){
+	cout << temp[i] << int(temp[i]);
+};
+cout << endl << a << temp <<endl;
+cout << g << h << endl ;
+
+return 0;
+};
