aa6a95d73a636efec02c9bde681f9a049052e41b
JannisHessel <jannishessel@gmx.de>
2018-10-15 15:03:40 +0200

    can go though the substrings of 10k long streams in decent speed

--- c++/geekforcehard/marysting/marystring.cpp
+++ c++/geekforcehard/marysting/marystring.cpp
@@ -5,13 +5,13 @@
 
 using namespace std;
 
-char str[100009];
+char str[100000];
 long long querys[100000];
-long long pos1[100009];
-long long pos2[100009];
-long long adress[100009];
+char * pos1[100000];//storage for the stings that nat not the current substrings
+char * pos2[100000];//storage for all the current substings
+char ** adress[100000];//points in pos1 where the stings differ
 
-
+/*
 
 int count(long length) {
     int c = 0;
@@ -29,32 +29,22 @@ int count(long length) {
     return c;
 }
 
-long letters (long length) {
 
-    if( length == 0 ){ return 0; }
-    if ( substr[length - 1] == 122 ) {
-
-        return letters(--length);
-
-    }
-
-    substr[length - 1]++;
-    return length;
-}
-
-
+*/
 
 
 
 int main(){
 
-substr[0] = 97;
 long n,q;
-ifstream mstr;
-mstr.open("query.txt" , ios::in);
 long length = 0;
 long long number = 0;
 long cq = 0;
+
+
+
+ifstream mstr;
+mstr.open("query.txt" , ios::in);
 
 mstr >> n;
 mstr >> q;
@@ -67,17 +57,79 @@ long i=0;
 
 while(mstr >> querys[i++]){}
 
+mstr.close();
+
 sort(querys,querys + q);
 
+pos2[0] = &str[0];
+for (long i = 1 ; i < n ; i++) {
+    pos2[i] = pos2[0] + i;
+}
+
+adress[0] = &pos1[0];
+
+char **place1, **place2, **end;
+char c = 97;
+place2 = &pos2[0] + n;
+
+while (true) {
+
+    place1 = adress[length];
+
+    end = place2;
+    place2 = &pos2[0];
 
 
+    for(char **i = &pos2[0] ; i < end ; i++){
+        if( *i + length >= &str[0] + n ){
+            break;
+        }
+        if ( *(*i + length) == c ){
+            *(place2++) = *i;
+        }else {
+            *(place1++) = *i;
+        }
 
-while (length >= 0){
-   
+    }
+
+    while(place2 == &pos2[0]){
+        if ( c == 122 ){
+            if (--length < 0){
+                cout << "done" << endl;
+                goto allfound;
+            }
+            c = *(pos2[0] + length);
+
+        }
+        c++;
+        end = place1;
+        place1 = adress[length];
+
+        for (char **i = adress[length] ; i < end ; i++){
+            if ( *(*i + length) == c ){
+                *(place2++) = *i;
+            }else {
+                *(place1++) = *i;
+            }
+        }
+
+    }
+    if (++number == querys[cq]){
+        cout << "got one" << endl;
+        cq++;
+    }
+
+    
+    if(cq == q ){break;}
+    adress[++length] = place1;
+    c = 97;
+    
 
 }
-while ( cq++ < q ){
-    cout << -1 << endl;
+allfound:
+
+while ( cq++ < q ) {
+   cout << -1 << endl;
 }
 
 return 0;
