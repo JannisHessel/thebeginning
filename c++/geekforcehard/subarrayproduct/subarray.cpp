f83ab3d002fab653e06418a00b67e38c1cd3c9e2
JannisHessel <jannishessel@gmx.de>
2018-11-20 13:43:40 +0100

    updatet version with the demandet numerical range -> other ones required numbers larger than 64 bit to handle numbers up to 10⁵*10¹⁵

--- c++/geekforcehard/subarrayproduct/subarray.cpp
+++ c++/geekforcehard/subarrayproduct/subarray.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <cmath>
 
 using namespace std;
 
@@ -13,7 +14,7 @@ int main() {
 
     long n;
     long long k;
-    long se[2] = {0,1};//start end
+    long se[2] = {0,0};//start end
 
 
     ifstream mstr;
@@ -29,23 +30,27 @@ int main() {
     mstr.close();
 
     long long sum = 0;
-    unsigned long long product = arr[0];
+    long long product = 1;
 
-    while (true) {
 
-        while(product < k) {
+    while (se[1] < n) {
 
+
+        while (product <= k / arr[se[1]] && !(k % arr[se[1]] == 0 && product == k / arr[se[1]])) {
+
+            product *= arr[se[1]++];
             if(se[1] == n) {
                 goto end;
             }
-
-            product = product*arr[se[1]++];
-
         }
+        sum += se[1] - se[0];
 
-
-        product = product / arr[se[0]++];
-        sum = sum + se[1] - se[0];
+        if (product == 1) {
+            se[0]++;
+            se[1]++;
+        } else {
+            product /= arr[se[0]++];
+        }
     }
 
 
